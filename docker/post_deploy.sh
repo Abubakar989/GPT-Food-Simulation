b23f14cc821a4401b3f5652d73e60799d164afa3
abubakar <abubakar.sadiq@wonderbits.net>
2025-04-29 14:08:11 +0200

    update docker compose

--- docker/post_deploy.sh
+++ docker/post_deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo " Applying Django database migrations..."
-docker-compose -f docker-compose-prod.yml exec web python manage.py migrate --noinput
+docker compose -f docker-compose-prod.yml exec web python manage.py migrate --noinput
 
 echo " Collecting static files..."
-docker-compose -f docker-compose-prod.yml exec web python manage.py collectstatic --noinput
+docker compose -f docker-compose-prod.yml exec web python manage.py collectstatic --noinput
 
 echo " Running conversation simulation..."
-docker-compose -f docker-compose-prod.yml exec web python manage.py simulate_conversations
+docker compose -f docker-compose-prod.yml exec web python manage.py simulate_conversations
 
 echo " Post-deployment tasks completed!"
